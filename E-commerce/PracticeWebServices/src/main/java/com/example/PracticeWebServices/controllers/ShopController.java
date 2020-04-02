600872894f0e15bf1e7e221ecf816bf6c0571cc0
kareem Magdy <kareemmagdy1@yahoo.com>
2020-04-02 21:25:09 +0200

    Test DB

--- E-commerce/PracticeWebServices/src/main/java/com/example/PracticeWebServices/controllers/ShopController.java
+++ E-commerce/PracticeWebServices/src/main/java/com/example/PracticeWebServices/controllers/ShopController.java
@@ -1,9 +1,15 @@
 package com.example.PracticeWebServices.controllers;
 
 
+import com.example.PracticeWebServices.domain.Shop;
 import com.example.PracticeWebServices.repositories.ShopRepository;
+import com.example.PracticeWebServices.service.ShopService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(ShopController.BASE_URL)
@@ -11,11 +17,22 @@ public class ShopController {
 
     public static final String BASE_URL="/shops";
 
-    private final ShopRepository shopRepository;
+    private final ShopService shopService;
 
-    public ShopController(ShopRepository shopRepository) {
-        this.shopRepository = shopRepository;
+
+    public ShopController(ShopService shopService, UserController userController) {
+        this.shopService = shopService;
     }
 
+
+    @GetMapping()
+    public List<Shop> getAllShops(){
+        return shopService.findAllShops();
+    }
+
+    @GetMapping("/{id}")
+    public Shop getShopById(@PathVariable Integer id){
+        return shopService.findShopById(id);
+    }
 
 }
--- E-commerce/PracticeWebServices/src/main/java/com/example/PracticeWebServices/domain/Shop.java
+++ E-commerce/PracticeWebServices/src/main/java/com/example/PracticeWebServices/domain/Shop.java
@@ -2,10 +2,7 @@ package com.example.PracticeWebServices.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -14,7 +11,7 @@ public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
      Integer shopId;
-//    @ManyToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "storeOwnerId",referencedColumnName = "storeOwnerId")
-//    StoreOwner storeOwner;
+
+
+
 }
